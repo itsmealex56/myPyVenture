9c6780937f61dc7b686b410c118b25eeb094ce35
itsmealex56 <waing.alex@gmail.com>
2019-07-07 10:23:42 +0800

    Early Chapters
    Early chapters journey

---
+++ Chapter 1/Phrase-O-Matic.py
@@ -0,0 +1,27 @@
+import random
+import datetime
+
+verbs = ['Leverage', 'Sync', 'Target',
+'Gamify', 'Offline', 'Crowd-sourced',
+'24/7', 'Lean-in', '30, 000 foot']
+
+adjectives = ['A/B Tested', 'Freemium',
+'Hyperlocal', 'Siloed', 'B-to-B',
+'Oriented', 'Cloud-based',
+'API-based']
+
+nouns = ['Early Adopter', 'Low-hanging Fruit',
+'Pipeline', 'Splash Page', 'Productivity',
+'Process', 'Tipping Point', 'Paradigm']
+
+verb = random.choice(verbs)
+adjective = random.choice(adjectives)
+noun = random.choice(nouns)
+
+phrase = verb + ' ' + adjective + ' ' + noun
+
+# date = datetime.date.today()
+
+print(phrase)
+print(datetime.date.today())
+
---
+++ Chapter 2/dogAgeCalcu.py
@@ -0,0 +1,7 @@
+dog_name = input('Name of your dog: ')
+dog_age = input("Your dog's age: ")
+calc_age = int(dog_age) * 7
+
+print(dog_name + "'s age is " + str(calc_age))
+
+print(dog_name, "'s age is", calc_age)
