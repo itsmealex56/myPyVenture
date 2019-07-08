e64ebcc4d928a10033bfd8e8e375225d2d5d0e8d
itsmealex56 <waing.alex@gmail.com>
2019-07-08 09:36:45 +0800

    Chapter 3
    old and refactored code for rock, paper and scissors or Jack-en-Poy

---
+++ Chapter 3/jackenpoy-refactored.py
@@ -0,0 +1,28 @@
+import random
+
+random_choice = random.randint(0,2)
+
+if random_choice == 0:
+    computer_choice = 'rock'
+elif random_choice == 1:
+    computer_choice = 'paper'
+else:
+    computer_choice = 'scissors'
+
+my_choice = input('Rock, Paper, or Scissors? ')
+
+if computer_choice == my_choice:
+    winner = 'Tie'
+elif computer_choice == 'rock' and my_choice == 'scissors':
+    winner = 'Computer'
+elif computer_choice == 'paper' and my_choice == 'rock':
+    winner = 'Computer'
+elif computer_choice == 'scissors' and my_choice =='paper':
+    winner = 'Computer'
+else:
+    winner = 'User'
+
+if winner == 'Tie':
+    print('Both tied! Computer also chose '+ computer_choice)
+else:
+    print('The', winner,'wins! the computer chose '+ computer_choice)
--- Chapter 3/jackenpoy.py
+++ Chapter 3/jackenpoy.py
@@ -9,4 +9,29 @@ elif random_choice == 1:
 else:
     computer_choice = 'scissors'
 
-print('Computer has pick', computer_choice)
+my_choice = input('Rock, Paper, or Scissors? ')
+while my_choice != "":
+    if my_choice == 'rock':
+        if computer_choice == 'scissors':
+            print('You chose rock and computer chose scissors...You won')
+        elif computer_choice == 'rock':
+            print('You chose rock and computer chose rock...You are both tied')
+        else:
+            print('You chose rock and computer chose paper...You lose')
+    elif my_choice == 'paper':
+        if computer_choice == 'scissors':
+            print('You chose paper and computer chose scissors...You lose')
+        elif computer_choice == 'rock':
+            print('You chose paper and computer chose rock...You won')
+        else:
+            print('You chose paper and computer chose paper...You are both tied')
+    else:
+        if computer_choice == 'scissors':
+            print('You chose scissors and computer chose scissors...You are both tied')
+        elif computer_choice == 'rock':
+            print('You chose scissors and computer chose rock...You lose')
+        else:
+            print('You chose scissors and computer chose paper...You won')
+
+
+    print('Computer has pick', computer_choice)
